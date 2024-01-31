ab1a56b349ee3dfbad69256741da6e9ab9e043db
柳树怪 <wolliw@163.com>
2024-01-31 15:34:01 +0800

    Initial commit

---
+++ src/main/java/com/fairingstudio/jtunes/Main.java
@@ -0,0 +1,27 @@
+package com.fairingstudio.jtunes;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+import io.datafx.controller.flow.Flow;
+import io.datafx.controller.flow.container.DefaultFlowContainer;
+import io.datafx.controller.flow.context.FXMLViewFlowContext;
+import io.datafx.controller.flow.context.ViewFlowContext;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("FS JTunes");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/fairingstudio/jtunes/MainController.java
@@ -0,0 +1,8 @@
+package com.fairingstudio.jtunes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+}
