d203a99ffba616c1d12ffbdc39b4898a5ea92acf
柳树怪 <wolliw@163.com>
2024-01-31 15:34:58 +0800

    UI lib

--- src/main/java/com/fairingstudio/jtunes/Main.java
+++ src/main/java/com/fairingstudio/jtunes/Main.java
@@ -1,27 +1,92 @@
 package com.fairingstudio.jtunes;
 
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+import com.jfoenix.assets.JFoenixResources;
+import com.jfoenix.controls.JFXDecorator;
+import com.jfoenix.svg.SVGGlyph;
+import io.datafx.controller.flow.FlowException;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.sql.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import io.datafx.controller.flow.Flow;
 import io.datafx.controller.flow.container.DefaultFlowContainer;
 import io.datafx.controller.flow.context.FXMLViewFlowContext;
 import io.datafx.controller.flow.context.ViewFlowContext;
+import javafx.stage.StageStyle;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
+
+    @FXMLViewFlowContext
+    private ViewFlowContext flowContext;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage stage) throws IOException, FlowException {
+
+
+        Flow flow = new Flow(MainController.class);
+
+        DefaultFlowContainer container = new DefaultFlowContainer();
+        flowContext = new ViewFlowContext();
+        flowContext.register("Stage", stage);
+        flow.createHandler(flowContext).start(container);
+
+
+
+        JFXDecorator decorator = new JFXDecorator(stage,container.getView() ,false,false,true);
+        decorator.setCustomMaximize(true);
+
+
+        decorator.setGraphic(new SVGGlyph(""));
+        stage.setMinWidth(600);
+        stage.setMinHeight(400);
+
+
         stage.setTitle("FS JTunes");
+
+        double width = 600;
+        double height = 400;
+        try {
+            Rectangle2D bounds = Screen.getScreens().get(0).getBounds();
+            width = bounds.getWidth() / 2;
+            height = bounds.getHeight() / 2;
+        }catch (Exception e){ }
+
+        Scene scene = new Scene(decorator, width, height);
+        scene.setFill(Color.TRANSPARENT);
+
+        final ObservableList<String> stylesheets = scene.getStylesheets();
+        stylesheets.addAll(JFoenixResources.load("css/jfoenix-fonts.css").toExternalForm(),
+                JFoenixResources.load("css/jfoenix-design.css").toExternalForm(),
+                Main.class.getClassLoader().getResource("com/fairingstudio/jtunes/css/main-style.css").toExternalForm());
+
+
+
         stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.getIcons().add(new Image("assets/img/logo.png"));
+
         stage.show();
     }
 
     public static void main(String[] args) {
+
         launch();
     }
+
+
+
 }
--- src/main/java/com/fairingstudio/jtunes/MainController.java
+++ src/main/java/com/fairingstudio/jtunes/MainController.java
@@ -1,8 +1,68 @@
 package com.fairingstudio.jtunes;
 
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+import com.jfoenix.controls.JFXHamburger;
+import io.datafx.controller.FXMLController;
+import javafx.animation.Transition;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+import javax.annotation.PostConstruct;
+import java.text.SimpleDateFormat;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.SimpleFormatter;
 
+@FXMLController(value = "Main.fxml", title = "Material Design Example")
+public class MainController {
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private JFXHamburger menu_ham;
+
+    public void changelabel(String text){
+        label1.setText(text);
+    }
+
+    @PostConstruct
+    public void init(){
+        SerialPort[] serialPorts = SerialPort.getCommPorts();
+        for (SerialPort port:serialPorts) {
+            System.out.println("串口名称:" + port.getSystemPortName());
+            System.out.println("串口类型:" + port.getPortDescription());
+            System.out.println("串口完整类型:" + port.getDescriptivePortName());
+        }
+        serialPorts[0].addDataListener(mainDataListener);
+
+        Timer timer = new Timer(true);
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                //System.out.println( System.currentTimeMillis());
+                Platform.runLater(()->{
+                    label1.setText(String.valueOf(new SimpleDateFormat("HH:mm:ss").format(System.currentTimeMillis())));
+                });
+
+
+            }
+        };
+        timer.schedule(task,500,500);
+    }
+    SerialPortDataListener mainDataListener = new SerialPortDataListener() {
+        @Override
+        public int getListeningEvents() {
+            return 0;
+        }
+
+        @Override
+        public void serialEvent(SerialPortEvent serialPortEvent) {
+
+        }
+    };
 }
